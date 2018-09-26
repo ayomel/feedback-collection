45e9ad7c43bc60bd386b3f3d959ab8d6b73d4235
Mel <mhunter11@users.noreply.github.com>
2018-09-26 00:03:44 -0400

    reducers

---
+++ client/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import  { BrowserRouter, Route } from 'react-router-dom'
+
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveryNew = () => <h2>SurveryNew</h2>
+const Landing = () => <h2>Landing</h2>
+
+const App = () => {
+  return (
+    <div>
+      Hi there
+    </div>
+  )
+}
+
+export default App;
